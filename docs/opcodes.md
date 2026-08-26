1f2737f34c63318e226c7b54b79305d72ff08ad9
Milk-Cool <coder.tomsk@gmail.com>
2026-08-26 08:38:23 +0300

    sembly!!

--- docs/opcodes.md
+++ docs/opcodes.md
@@ -6,41 +6,41 @@ Most of these are taken from CHIP-8
 
 |Bits|Sembly|Description|Argument bits count|
 |-|-|-|-|
-|00010000XXXXXXXX||Clear the screen|0|
-|00010001XXXXXXXX||Return from function|0|
-|10000||Jump to address|11|
-|10001||Cal function at address|11|
-|10010||Skip opcode if register equals to 8bit number|3+8=11|
-|10011||Skip opcode if register doesn't equal to 8bit number|3+8=11|
-|01000000XX||Skip opcode if register equals another register|3+3=6|
-|01000001XX||Skip opcode if register doesn't equal another register|3+3=6|
-|10100||Set register to 8bit number|3+8=11|
-|10101||Add 8bit number to register, set R7 to overflow|3+8=11|
-|01000010XX||Add another register to register, set R7 to overflow|3+3=6|
-|01000011XX||Subtract another register from register, set R7 to underflow|3+3=6|
-|01000100XX||Set register to another register minus it, set R7 to underflow|3+3=6|
-|01000101XX||Set register to the value of another register|3+3=6|
-|01000110XX||Set register to the value of it OR another register|3+3=6|
-|01000111XX||Set register to the value of it AND another register|3+3=6|
-|01001000XX||Set register to the value of it XOR another register|3+3=6|
-|01001001XX||Set register to the value of another register, shift right and set R7 to the bit shifted out|3+3=6|
-|01001010XX||Set register to the value of another register, shift left and set R7 to the bit shifted out|3+3=6|
-|10110||Set P to address|11|
-|10111||Set P to address plus R0|11|
-|11000||Set register to random byte AND specified byte|3+8=11|
-|1111||Draw 8 by number plus 1 sprite, loaded from P, at X = register Y = another register|3+3+6=12|
-|00100000XXXXX||Skip opcode if button from register is pressed|3|
-|00100001XXXXX||Skip opcode if button from register is not pressed|3|
-|00100010XXXXX||Skip opcode if button from value is pressed|3|
-|00100011XXXXX||Skip opcode if button from value is not pressed|3|
-|00100100XXXXX||Block execution until button is pressed, then store button number to register|3|
-|00100101XXXXX||Set register to value of the delay timer|3|
-|00100110XXXXX||Set value of the delay timer to register|3|
-|00100111XXXXX||Set value of the sound timer to register, background is white until it reaches 0|3|
-|00101000XXXXX||Add register value to P|3|
-|00101001XXXXX||Store value in register to P, P+1 and P+2 as hundreds, tens and ones respectively|3|
-|00101010XXXXX||Store values from R0-RX to memory from P and add X+1 to P|3|
-|00101011XXXXX||Read values to R0-RX from memory from P and add X+1 to P|3|
+|00010000XXXXXXXX|`clr`|Clear the screen|0|
+|00010001XXXXXXXX|`ret`|Return from function|0|
+|10000|`jump A`, `jmp A`|Jump to address|11|
+|10001|`call A`|Call function at address|11|
+|10010|`seq R N`|Skip opcode if register equals to 8bit number|3+8=11|
+|10011|`sneq R N`|Skip opcode if register doesn't equal to 8bit number|3+8=11|
+|01000000XX|`seq R R`|Skip opcode if register equals another register|3+3=6|
+|01000001XX|`sneq R R`|Skip opcode if register doesn't equal another register|3+3=6|
+|10100|`set R N`, `mov R N`|Set register to 8bit number|3+8=11|
+|10101|`add R N`|Add 8bit number to register, set R7 to overflow|3+8=11|
+|01000010XX|`add R R`|Add another register to register, set R7 to overflow|3+3=6|
+|01000011XX|`sub R R`|Subtract another register from register, set R7 to underflow|3+3=6|
+|01000100XX|`subr R R`|Set register to another register minus it, set R7 to underflow|3+3=6|
+|01000101XX|`set R R`, `mov R R`|Set register to the value of another register|3+3=6|
+|01000110XX|`or R R`|Set register to the value of it OR another register|3+3=6|
+|01000111XX|`and R R`|Set register to the value of it AND another register|3+3=6|
+|01001000XX|`xor R R`|Set register to the value of it XOR another register|3+3=6|
+|01001001XX|`bsr R R`|Set register to the value of another register, shift right and set R7 to the bit shifted out|3+3=6|
+|01001010XX|`bsl R R`|Set register to the value of another register, shift left and set R7 to the bit shifted out|3+3=6|
+|10110|`setp A`|Set P to address|11|
+|10111|`setp0 A`|Set P to address plus R0|11|
+|11000|`andr R N`|Set register to random byte AND specified byte|3+8=11|
+|1111|`draw R R N`|Draw 8 by number plus 1 sprite, loaded from P, at X = register Y = another register|3+3+6=12|
+|00100000XXXXX|`sbp R`|Skip opcode if button from register is pressed|3|
+|00100001XXXXX|`sbnp R`|Skip opcode if button from register is not pressed|3|
+|00100010XXXXX|`sbp N`|Skip opcode if button from value is pressed|3|
+|00100011XXXXX|`sbnp N`|Skip opcode if button from value is not pressed|3|
+|00100100XXXXX|`setb R`, `movb R`|Block execution until button is pressed, then store button number to register|3|
+|00100101XXXXX|`setdr R`, `movdr R`|Set register to value of the delay timer|3|
+|00100110XXXXX|`setd R`, `movd R`|Set value of the delay timer to register|3|
+|00100111XXXXX|`sets R`, `movs R`|Set value of the sound timer to register, background is white until it reaches 0|3|
+|00101000XXXXX|`addp R`|Add register value to P|3|
+|00101001XXXXX|`dcm R`|Store value in register to P, P+1 and P+2 as hundreds, tens and ones respectively|3|
+|00101010XXXXX|`psav R`|Store values from R0-RX to memory from P and add X+1 to P|3|
+|00101011XXXXX|`plod R`|Read values to R0-RX from memory from P and add X+1 to P|3|
 
 ## Leftovers
 |Bits|Argument bits count
---
+++ sembly.ts
@@ -0,0 +1,240 @@
+#!/usr/bin/env node
+
+import { parseArgs } from "util";
+import fs from "fs";
+import { join } from "path";
+import { tmpdir } from "os";
+import { randomBytes } from "crypto";
+import { spawnSync } from "child_process";
+
+const { positionals, values } = parseArgs({
+    allowPositionals: true,
+    options: {
+        output: {
+            type: "string",
+            short: "o",
+            default: "-"
+        },
+        raw: {
+            type: "boolean",
+            short: "r",
+            default: false
+        },
+        pad: {
+            type: "string",
+            short: "p",
+            default: "512"
+        },
+        ln: {
+            type: "boolean",
+            short: "l",
+            default: false
+        },
+        url: {
+            type: "boolean",
+            short: "u",
+            default: false
+        }
+    }
+});
+
+if(positionals.length === 0) {
+    console.error("No input files specified, exiting!");
+    process.exit(1);
+}
+const code = fs.readFileSync(positionals[0], "utf-8").split("\n").map(x => x.split(";")[0].trim());
+
+const NUMBER_REGEX = /^(\d+|0x[0-9a-fA-F]+|0b[01]+)$/;
+const parseNumber = (str: string) => str.startsWith("0b")
+    ? parseInt(str.slice(2), 2)
+    : parseInt(str); // hex values are handled by `parseInt` correctly
+const REGISTER_REGEX = /^R[0-7]$/;
+const MARKER_REGEX = /^\w+:$/;
+const POINTER_REGEX = /^@\w+$/;
+const out: (number | ["address", number] | ["marker" | "pointer", string])[] = [];
+type TokenType = "number" | "register" | "marker" | "pointer";
+class ExpectationError extends Error {};
+class DerivationError extends Error {};
+class AddressError extends Error {};
+class PointerError extends Error {};
+let n = 0;
+const expect = (str: string, mask: string): [TokenType, any] => {
+    if(mask.includes("n") && str.match(NUMBER_REGEX)) return ["number", parseNumber(str)];
+    else if(mask.includes("r") && str.match(REGISTER_REGEX)) return ["register", parseInt(str[1])];
+    else if(mask.includes("m") && str.match(MARKER_REGEX)) return ["marker", str.slice(0, -1)];
+    else if(mask.includes("p") && str.match(POINTER_REGEX)) return ["pointer", str.slice(1)];
+    throw new ExpectationError(`Unknown or wrong argument type at line ${n}`);
+};
+const derive = <T>(args: string[], cases: Record<string, (values: any[]) => T>) => {
+    for(const [k, v] of Object.entries(cases)) {
+        try {
+            if(args.length !== k.length) continue;
+            let i = 0, values = [];
+            for(const mask of k.split("")) values.push(expect(args[i++], mask)[1]);
+            return v(values);
+        } catch(e) {
+            if(!(e instanceof ExpectationError)) throw e;
+        };
+    }
+    throw new DerivationError(`Unknown or wrong argument set at line ${n}`);
+};
+const revBits = (n: number) => {
+    let o = 0;
+    for(let i = 0; i < 8; i++) {
+        o <<= 1;
+        o |= (n >> i) & 1;
+    }
+    return o;
+};
+for(const line of code) {
+    n++;
+    const split = line.split(/\s+/g);
+    if(split.length < 1) continue;
+    const instr = split[0];
+    const args = split.slice(1);
+    try {
+        derive([instr], {
+            n: v => out.push(["address", v[0]]),
+            m: v => out.push(["marker", v[0]]),
+        });
+        continue;
+    } catch(e) {
+        if(!(e instanceof DerivationError)) throw e;
+    }
+    if(instr === "clr") out.push(0b00010000, 0b00000000);
+    else if(instr === "ret") out.push(0b00010001, 0b00000000);
+    else if(instr === "jump"|| instr === "jmp") derive(args, {
+        p: v => out.push(0b10000000, ["pointer", v[0]]),
+        n: v => out.push(0b10000000 | (v[0] >> 8), v[0]),
+    });
+    else if(instr === "call") derive(args, {
+        p: v => out.push(0b10001000, ["pointer", v[0]]),
+        n: v => out.push(0b10001000 | (v[0] >> 8), v[0]),
+    });
+    else if(instr === "seq") derive(args, {
+        rn: v => out.push(0b10010000 | v[0], v[1]),
+        rr: v => out.push(0b01000000, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "sneq") derive(args, {
+        rn: v => out.push(0b10011000 | v[0], v[1]),
+        rr: v => out.push(0b01000001, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "set" || instr === "mov") derive(args, {
+        rn: v => out.push(0b10100000 | v[0], v[1]),
+        rr: v => out.push(0b01000101, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "add") derive(args, {
+        rn: v => out.push(0b10101000 | v[0], v[1]),
+        rr: v => out.push(0b01000010, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "sub") derive(args, {
+        rn: v => out.push(0b10101000 | v[0], -v[1]),
+        rr: v => out.push(0b01000011, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "subr") derive(args, {
+        rr: v => out.push(0b01000100, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "or") derive(args, {
+        rr: v => out.push(0b01000110, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "and") derive(args, {
+        rr: v => out.push(0b01000111, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "xor") derive(args, {
+        rr: v => out.push(0b01001000, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "bsr") derive(args, {
+        rr: v => out.push(0b01001001, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "bsl") derive(args, {
+        rr: v => out.push(0b01001010, (v[0] << 3) | v[1]),
+    });
+    else if(instr === "setp" || instr === "movp") derive(args, {
+        p: v => out.push(0b10110000, ["pointer", v[0]]),
+        n: v => out.push(0b10110000 | (v[0] >> 8), v[0]),
+    });
+    else if(instr === "setp0" || instr === "movp0") derive(args, {
+        p: v => out.push(0b10111000, ["pointer", v[0]]),
+        n: v => out.push(0b10111000 | (v[0] >> 8), v[0]),
+    });
+    else if(instr === "andr") derive(args, {
+        rn: v => out.push(0b11000000 | v[0], v[1]),
+    });
+    else if(instr === "draw") derive(args, {
+        rrn: v => out.push(0b11110000 | (v[0] << 1) | (v[1] >> 2), ((v[1] & 3) << 6) | v[2]),
+    });
+    else if(instr === "sbp") derive(args, {
+        r: v => out.push(0b00100000, v[0]),
+        n: v => out.push(0b00100010, v[0]),
+    });
+    else if(instr === "sbnp") derive(args, {
+        r: v => out.push(0b00100001, v[0]),
+        n: v => out.push(0b00100011, v[0]),
+    });
+    else if(instr === "setb" || instr === "movb") derive(args, {
+        r: v => out.push(0b00100100, v[0]),
+    });
+    else if(instr === "setdr" || instr === "movdr") derive(args, {
+        r: v => out.push(0b00100101, v[0]),
+    });
+    else if(instr === "setd" || instr === "movd") derive(args, {
+        r: v => out.push(0b00100110, v[0]),
+    });
+    else if(instr === "sets" || instr === "movs") derive(args, {
+        r: v => out.push(0b00100111, v[0]),
+    });
+    else if(instr === "addp") derive(args, {
+        r: v => out.push(0b00101000, v[0]),
+    });
+    else if(instr === "dcm") derive(args, {
+        r: v => out.push(0b00101001, v[0]),
+    });
+    else if(instr === "psav") derive(args, {
+        r: v => out.push(0b00101010, v[0]),
+    });
+    else if(instr === "plod") derive(args, {
+        r: v => out.push(0b00101011, v[0]),
+    });
+    else if(instr === "db") out.push(...args.map(parseNumber));
+    else if(instr === "dbr") out.push(...args.map(x => revBits(parseNumber(x))));
+}
+
+let buf = Buffer.alloc(0);
+for(const el of out) {
+    if(typeof el === "number")
+        buf = Buffer.concat([buf, Buffer.from([el & 0xff])]);
+    else if(el[0] === "address") {
+        if(buf.length > el[1]) throw new AddressError(`Can't fit to ${el[0]} bytes since preceding code is larger!`);
+        buf = Buffer.concat([buf, Buffer.alloc(el[1] - buf.length)]);
+    } else if(el[0] === "pointer") {
+        let n = 0, flag = false;
+        for(const subel of out) {
+            if(typeof subel === "number") n++;
+            else if(subel[0] === "address") n = subel[1];
+            else if(subel[0] === "pointer") n++;
+            else if(subel[0] === "marker" && subel[1] === el[1]) {
+                flag = true;
+                break;
+            }
+        }
+        if(!flag) throw new PointerError(`Marker not found: ${el[1]}`);
+        buf[buf.length - 1] |= n >> 8;
+        buf = Buffer.concat([buf, Buffer.from([n & 0xff])]);
+    }
+}
+
+if(!values.pad.match(/^\d+$/)) {
+    console.error("--pad seems to be invalid, exiting!");
+    process.exit(1);
+}
+
+if(values.raw) {
+    if(values.output === "-") process.stdout.write(buf);
+    else fs.writeFileSync(values.output, buf);
+} else {
+    const p = join(tmpdir(), "qr-" + randomBytes(8).toString("hex") + ".bin");
+    fs.writeFileSync(p, buf);
+    const options = [["u", values.url], ["l", values.ln]].filter(x => x[1]).map(x => x[0]).join("");
+    const { status } = spawnSync("qrboy-embed", [p, "-p", values.pad, "-o", values.output].concat(options ? ["-" + options] : []), { stdio: "inherit" });
+    if(status !== 0) process.exit(3);
+}
